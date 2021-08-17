dfce9feab29ce44e3120c3c947f27057efe51c38
healthcarefrontend <himchan9494@gmail.com>
2021-08-18 04:20:34 +0900

    props

---
+++ course/01/src/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  //   const headerStyle = { backgroundColor: "mediumblue", color: "#fff" };
+
+  return (
+    <header>
+      <h1>Groceries List</h1>
+    </header>
+  );
+};
+
+export default Header;
+
+// _rafce
