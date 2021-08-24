d7f36656497c6cb6bff7b4c273bcb9d6f0d58df4
healthcarefrontend <himchan9494@gmail.com>
2021-08-24 22:09:25 +0900

    custom hook

--- course/02/src/NewPost.js
+++ course/02/src/NewPost.js
@@ -1,7 +1,31 @@
-const NewPost = () => {
+const NewPost = ({
+  handleSubmit,
+  postTitle,
+  setPostTitle,
+  postBody,
+  setPostBody,
+}) => {
   return (
-    <main>
-      <h1>NewPost</h1>
+    <main className="NewPost">
+      <h2>NewPost</h2>
+      <form className="newPostForm" onSubmit={handleSubmit}>
+        <lable htmlFor="postTitle">Title :</lable>
+        <input
+          id="postTitle"
+          type="text"
+          required
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}
+        />
+        <lable htmlFor="postBody">Post :</lable>
+        <textarea
+          id="postBody"
+          required
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </main>
   );
 };
